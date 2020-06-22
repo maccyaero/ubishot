671989c2a674d1b10129a00c09998c55f141bd04
Wali Abbas <waliabbas@ymail.com>
2020-06-23 00:23:09 +1000

    Fixed error to get user inpiut from tkinter.entry

--- gui.py
+++ gui.py
@@ -1,14 +1,21 @@
 import tkinter
-
 window = tkinter.Tk()
 window.title("Ubishot")
 
 def update_label():
     tkinter.Label(window,text = "Please enter a valid directory").grid(row = 4, column = 4)
 
+def retrieve_input():
+    inputValue = textBox.get()
+    print(inputValue)
+
 tkinter.Label(window,text = "Please enter directory: ").grid(row=0, column = 4)
-tkinter.Entry(window).grid(row = 2, column = 4)
-tkinter.Button(window,text = "Change", command = update_label ).grid(row = 3, column = 3)
+textBox = tkinter.Entry(window)
+textBox.grid(row = 2, column = 4)
+tkinter.Button(window,text = "Change", command = retrieve_input ).grid(row = 3, column = 3)
+
+
+window.mainloop()
 
 
 
