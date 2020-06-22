afd8ad4736f39bd8d9aaf5da766c8e1ace403dd2
Syed Wali Abbas Zaidi <wali@Syeds-iMac.local>
2020-06-23 02:20:21 +1000

    moved all the code from screenshot.py to gui.py due to this unresolved error:partially initialized module 'screenshot' has no attribute 'validate' (most likely due to a circular import)

--- gui.py
+++ gui.py
@@ -1,18 +1,34 @@
-import tkinter
+import tkinter,os,sys
 window = tkinter.Tk()
 window.title("Ubishot")
 
 def update_label():
     tkinter.Label(window,text = "Please enter a valid directory").grid(row = 4, column = 4)
 
-def retrieve_input():
+def validate():
     inputValue = textBox.get()
     print(inputValue)
+    directory = inputValue
+    isDirectory = os.path.isdir(directory) #imported methos from OS module to check if entered directory is valid
+    print(isDirectory)
+
+    if isDirectory == False:
+        update_label()
+    else:
+        change_directory(directory)
+
+
+def change_directory(dir):
+    command = "defaults write com.apple.screencapture location " #Shell Coomad to change default screenshot location
+    command += dir #Appending the user entered directory to the command used to change screesnshot location
+    print("The final command passing to cli is : " + command)
+    os.system(command) #Runs the command on the shell.
+
 
 tkinter.Label(window,text = "Please enter directory: ").grid(row=0, column = 4)
 textBox = tkinter.Entry(window)
 textBox.grid(row = 2, column = 4)
-tkinter.Button(window,text = "Change", command = retrieve_input ).grid(row = 3, column = 3)
+tkinter.Button(window,text = "Change", command = validate ).grid(row = 3, column = 3)
 
 
 window.mainloop()
--- screenshot.py
+++ screenshot.py
@@ -1,20 +1,19 @@
-import os,sys,tkinter,gui
+import os,sys,gui
 
-command = "defaults write com.apple.screencapture location " #Shell Coomad to change default screenshot location
-
-
-while True:
-    directory = input("Enter the directory: ")
+def validate():
+    inputValue = gui.textBox.get()
+    print(inputValue)
+    directory = inputValue
     isDirectory = os.path.isdir(directory) #imported methos from OS module to check if entered directory is valid
 
-    if directory =='q': #user can press q to exit the program.
-        sys.exit()
-
     if isDirectory == False:
-        print('Please enter a valid directory or press q to exit.')
+        gui.update_label
     else:
-        break
+        change_directory(directory)
 
-command += directory #Appending the user entered directory to the command used to change screesnshot location
-print("The final command passing to cli is : " + command)
-os.system(command) #Runs the command on the shell.
+
+def change_directory(dir):
+    command = "defaults write com.apple.screencapture location " #Shell Coomad to change default screenshot location
+    command += dir #Appending the user entered directory to the command used to change screesnshot location
+    print("The final command passing to cli is : " + command)
+    os.system(command) #Runs the command on the shell.
